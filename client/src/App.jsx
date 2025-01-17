c596334ca8ec076dd3e94e346acc35bf0ed631e2
dhruv0111 <dhruvsharmaaugust2003@gmail.com>
2025-01-18 03:07:40 +0530

    create Header Compoenent with navgation bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,15 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Dashboard from './pages/Dashboard'
-import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Projects from './pages/Projects'
+import SignIn from './pages/Signin'
+import Header from './component/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />}  />
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+import flowbitePlugin from 'flowbite/plugin';
+
+
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}',
+    './node_modules/flowbite-react/**/*.js',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [
+    flowbitePlugin,
+   
+  ],
+};
