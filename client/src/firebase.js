b4f912f36bb8fdef43cd9f7e1505c7c9abd73b42
dhruv0111 <dhruvsharmaaugust2003@gmail.com>
2025-02-13 17:21:14 +0530

    add OAuth functionality

---
+++ client/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-blog-9292a.firebaseapp.com",
+  projectId: "mern-blog-9292a",
+  storageBucket: "mern-blog-9292a.firebasestorage.app",
+  messagingSenderId: "633603276414",
+  appId: "1:633603276414:web:5ae8194a4ea4ca8d89a22f"
+};
+
+// Initialize Firebase
+ export const app = initializeApp(firebaseConfig);
+
+ 
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { signInSuccess,signInStart, signInFailure } from "../redux/user/userSlice";
+import OAuth from "../component/OAuth";
 
 
 export default function SignIp() {
@@ -97,6 +98,7 @@ export default function SignIp() {
                 )
               }
             </Button>
+            <OAuth />
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Don&apos;t Have an account?</span>
