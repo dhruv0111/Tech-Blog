89dd00317d4044b6d577a76dc01076b3ac92cfc9
dhruv0111 <dhruvsharmaaugust2003@gmail.com>
2025-02-15 15:47:27 +0530

    create all Post, comment api route and post Ui functionality

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose  from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoute from './routes/auth.route.js';
+import postRoutes from './routes/post.route.js';
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -26,6 +27,7 @@ app.listen(3000,()=>{
 
 app.use('/api/user/', userRoutes);
 app.use('/api/auth', authRoute);
+app.use('/api/post', postRoutes);
 
 
 
